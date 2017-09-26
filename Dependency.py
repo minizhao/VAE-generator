86db912213bc6352cfa82b02c04a56b2b11df93e
minizhao <zhao844895171@gmail.com>
2017-09-26 14:52:19 +0800

    Create Dependency.py

---
+++ Dependency.py
@@ -0,0 +1,29 @@
+def add_dependency_to_vec(words_vec,sentences_data):
+  
+  """
+  参数说明
+ 
+  words_vec：词向量数据，有如下属性:( words_vec.vocab :表示包含的所以词的词典,\
+              如果 有个词 w 在words_vec.vocab中 ,可用 words_vec[w] 获得其词向量（100维）)
+  
+  sentences_data：文本数据包含8w条左右的数据，如下所示：
+  
+                  [ [1,3,6,7,9,0,0],
+                    [5,6,7,9,1,3,0],
+                    ....
+                    ....
+                    ....
+                  ]
+                 每句话由一个list表示，其中每一个数字表示词的编号
+                 
+  返回参数说明：
+  
+  added_dependency_words_vec：已经添加依存的词向量数据，数据类型同words_vec
+       
+  
+  """
+  
+  
+  pass
+
+  return added_dependency_vec
